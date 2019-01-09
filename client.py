d3add09677f52913fdaddcba169ff455b311a861
yoru <yoru@localhost.localdomain>
2019-01-10 00:19:43 +0200

    connection

--- client.py
+++ client.py
@@ -1,15 +1,35 @@
+#!/usr/bin/python  
+
 import os
 import sys
 import socket
 
-def main():
+def get_arg(arg, status, arr):
+    if arg in arr:
+        return arr[arr.index(arg) + 1]
+    else:
+        sys.exit(status)
+
+def interpret_args():
+    args = {}
+    args['host'] = get_arg('-h', 'ERR: No host to connect to', sys.argv)
+    args['port'] = get_arg('-p', 'ERR: No port specified', sys.argv)
+    return args
+
+def connection():
+    args = interpret_args()
+    print(args)
+
     s = socket.socket()         
-    host = socket.gethostname() 
-    port = 9998                
+    host = args['host'] 
+    port = args['port']
 
     s.connect((host, port))
     print(s.recv(1024))
-    s.close()      
+    s.close()
+
+def main():      
+    connection()
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -2,14 +2,30 @@ import os
 import sys
 import socket
 
-def main():
+def get_arg(arg, status, arr):
+    if arg in arr:
+        return arr[arr.index(arg) + 1]
+    else:
+        sys.exit(status)
+
+def interpret_args():
+    args = {}
+    args['port'] = get_arg('-p', 'ERR: No port specified', sys.argv)
+    return args
+
+def connection():
+    args = interpret_args()
+    port = args['port']
+
     s = socket.socket(socket.AF_INET)
-    print(socket.IPPROTO_IP)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind(("", 9998))
+    s.setsockopt(socket.IPPROTO_IP, socket.SO_REUSEADDR, 1)
+    s.bind(("", port))
     s.listen(1)
     (conn, address) = s.accept()
     print(conn, '\n', address)
+
+def main():
+    connection()
 
 if __name__ == '__main__':
     main()
